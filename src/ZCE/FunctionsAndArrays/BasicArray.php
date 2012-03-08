68d19aef110f02a997a5bc8859789346a2384224
Daniel Gomes <danielcesargomes@gmail.com>
2012-03-08 12:16:51 +0000

    added namespaces

---
+++ src/ZCE/FunctionsAndArrays/BasicArray.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace ZCE\FunctionsAndArrays;
+
+class BasicArray
+{
+    function __construct()
+    {
+        $data = array(
+            0 => 1,
+            3 => 2,
+            5 => 3,
+            2 => 5,
+            1 => 4
+        );
+
+        //order by keys
+        ksort($data);
+        $keysOrder = $data;
+
+        //order by values
+        asort($data);
+        $valuesOrder = $data;
+
+        var_dump($keysOrder);
+        var_dump($valuesOrder);
+    }
+}
+
+new \BasicArray();
---
+++ src/ZCE/PHPBasics/PHPBasics.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace ZCE\PHPBasics;
+
+class PHPBasics
+{
+    function __construct()
+    {
+
+    }
+
+    function __toString()
+    {
+        return "PHP Basics";
+    }
+}
+
+new \PHPBasics();
