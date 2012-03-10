3efbb88a27db004fb8c60d3f50c2fa864bf089fa
Daniel Gomes <me@danielcsgomes.com>
2012-03-10 23:30:49 +0000

    added resources and regex.php minor update to Functions.php

--- src/ZCE/FunctionsAndArrays/Functions.php
+++ src/ZCE/FunctionsAndArrays/Functions.php
@@ -5,9 +5,9 @@
  */
 function arguments()
 {
-    var_dump(func_num_args());
-    var_dump(func_get_arg(1));
-    var_dump(func_get_args());
+    var_dump(func_num_args()); // output 3
+    var_dump(func_get_arg(1)); // output 9
+    var_dump(func_get_args()); //output array(1.5,9,63)
 }
 
 arguments(1.5,9,63);
@@ -18,5 +18,5 @@ arguments(1.5,9,63);
 
 $funcSingleQuote    = create_function('$x','return $x*$x;');
 $funcDoubleQuote    = create_function("\$x", "return \$x*\$x;"); //with double quotes the dollar must be escaped
-var_dump($funcSingleQuote(10));
-var_dump($funcDoubleQuote(10));
+var_dump($funcSingleQuote(10)); //output 100
+var_dump($funcDoubleQuote(10)); //output 100
---
+++ src/ZCE/StringAndPatterns/RegEx.php
@@ -0,0 +1,6 @@
+<?php
+
+/**
+ * Validate email
+ */
+
