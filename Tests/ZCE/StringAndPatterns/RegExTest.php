12882ed96dda58ca2d67b4a8506c4362f2571562
Daniel Gomes <daniel.gomes@reset.pt>
2012-03-17 19:03:09 +0000

    reafctored some regular expressions, added Unit Tests

---
+++ Tests/ZCE/StringAndPatterns/RegExTest.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * This file is part of the ZCE Preparation Exam package.
+ *
+ * (c) Daniel Gomes <me@danielcsgomes.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Tests\ZCE\StringAndPatterns;
+
+use ZCE\StringsAndPatterns\RegEx;
+
+/**
+ * Tests for Regular Expressions
+ *
+ * @author Daniel Gomes <me@danielcsgomes.com>
+ */
+class RegExTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCellphone ()
+    {
+        $regEx = new RegEx();
+        $cellphone = "910000000";
+        $this->assertTrue($regEx->matchStringAndPattern(RegEx::$cellphone, $cellphone));
+        $cellphone = "+351930000000";
+        $this->assertTrue($regEx->matchStringAndPattern(RegEx::$cellphone, $cellphone));
+        $cellphone = "00351930000000";
+        $this->assertTrue($regEx->matchStringAndPattern(RegEx::$cellphone, $cellphone));
+        $cellphone = "35193000000";
+        $this->assertFalse($regEx->matchStringAndPattern(RegEx::$cellphone, $cellphone));
+        $cellphone = "3519300000";
+        $this->assertFalse($regEx->matchStringAndPattern(RegEx::$cellphone, $cellphone));
+        $cellphone = "3193000000";
+        $this->assertFalse($regEx->matchStringAndPattern(RegEx::$cellphone, $cellphone));
+    }
+
+    public function testZipCode ()
+    {
+        $regEx = new RegEx();
+        $zip_code = "2000";
+        $this->assertTrue($regEx->matchStringAndPattern(RegEx::$zip_code, $zip_code));
+        $zip_code = "2000-100";
+        $this->assertTrue($regEx->matchStringAndPattern(RegEx::$zip_code, $zip_code));
+        $zip_code = "295";
+        $this->assertFalse($regEx->matchStringAndPattern(RegEx::$zip_code, $zip_code));
+        $zip_code = "1000-00";
+        $this->assertFalse($regEx->matchStringAndPattern(RegEx::$zip_code, $zip_code));
+        $zip_code = "2000-00";
+        $this->assertFalse($regEx->matchStringAndPattern(RegEx::$zip_code, $zip_code));
+        $zip_code = "200-000";
+        $this->assertFalse($regEx->matchStringAndPattern(RegEx::$zip_code, $zip_code));
+        $zip_code = "2000 000";
+        $this->assertFalse($regEx->matchStringAndPattern(RegEx::$zip_code, $zip_code));
+    }
+
+    public function testEmail ()
+    {
+        $regEx = new RegEx();
+        $email = "me@danielcsgomes.com";
+        $this->assertTrue($regEx->matchStringAndPattern(RegEx::$email, $email));
+        $email = "danielcsgomes.com";
+        $this->assertFalse($regEx->matchStringAndPattern(RegEx::$email, $email));
+        $email = "me@danielcsgomes.c";
+        $this->assertFalse($regEx->matchStringAndPattern(RegEx::$email, $email));
+        $email = "m@danielcsgomes.com";
+        $this->assertFalse($regEx->matchStringAndPattern(RegEx::$email, $email));
+        $email = "me@d.com";
+        $this->assertFalse($regEx->matchStringAndPattern(RegEx::$email, $email));
+    }
+
+    public function testPhone ()
+    {
+        $regEx = new RegEx();
+        $phone = "210000000";
+        $this->assertTrue($regEx->matchStringAndPattern(RegEx::$phone, $phone));
+        $phone = "+351230000000";
+        $this->assertTrue($regEx->matchStringAndPattern(RegEx::$phone, $phone));
+        $phone = "00351230000000";
+        $this->assertTrue($regEx->matchStringAndPattern(RegEx::$phone, $phone));
+        $phone = "35123000000";
+        $this->assertFalse($regEx->matchStringAndPattern(RegEx::$phone, $phone));
+        $phone = "3512300000";
+        $this->assertFalse($regEx->matchStringAndPattern(RegEx::$phone, $phone));
+        $phone = "3123000000";
+        $this->assertFalse($regEx->matchStringAndPattern(RegEx::$phone, $phone));
+    }
+
+}
--- src/ZCE/StringsAndPatterns/RegEx.php
+++ src/ZCE/StringsAndPatterns/RegEx.php
@@ -17,10 +17,10 @@ namespace ZCE\StringsAndPatterns;
  */
 class RegEx
 {
-    static $zip_code = '/\d{4}-\d{3}|\d{4}/';
+    static $zip_code = '/\d{4}-\d{3}|^\d{4}$/m';
     static $email = '/[a-zA-Z0-9._]{2,}@[a-zA-Z0-9]{2,}\.[a-zA-Z]{2,4}/';
-    static $phone = '/(00351|\+351)2\d{8}|2\d{8}/';
-    static $cellphone = '/(00351|\+351)9\d{8}|9\d{8}/';
+    static $phone = '/(00351|\+351)2\d{8}|^(2\d{8})$/m';
+    static $cellphone = '/(00351|\+351)9\d{8}|^(9\d{8})$/m';
 
     public function matchStringAndPattern($pattern, $string)
     {
@@ -31,18 +31,3 @@ class RegEx
 
     }
 }
-
-$regex = new RegEx();
-
-var_dump($regex->matchStringAndPattern(RegEx::$zip_code,"2000-001")); //true
-var_dump($regex->matchStringAndPattern(RegEx::$zip_code,"2000")); //true
-var_dump($regex->matchStringAndPattern(RegEx::$email,"me@danielcsgomes.com")); //true
-var_dump($regex->matchStringAndPattern(RegEx::$email,"m@d.c")); //false
-var_dump($regex->matchStringAndPattern(RegEx::$email,"me@fake.c")); //false
-var_dump($regex->matchStringAndPattern(RegEx::$email,"me@f.com")); //false
-var_dump($regex->matchStringAndPattern(RegEx::$phone,"999999999")); //false
-var_dump($regex->matchStringAndPattern(RegEx::$phone,"299999999")); //true
-var_dump($regex->matchStringAndPattern(RegEx::$phone,"00351299999999")); //true
-var_dump($regex->matchStringAndPattern(RegEx::$phone,"299999999")); //true
-var_dump($regex->matchStringAndPattern(RegEx::$cellphone,"999999999")); //true
-var_dump($regex->matchStringAndPattern(RegEx::$cellphone,"+351999999999")); //true
