15226b7ae8b7b1c40a8a5afc6bbd1e67cba09ed2
Daniel Gomes <me@danielcsgomes.com>
2012-05-14 00:20:24 +0100

    added licence

--- src/ZCE/PHPBasics/PHPBasics.php
+++ src/ZCE/PHPBasics/PHPBasics.php
@@ -1,6 +1,12 @@
 <?php
-
-
+/**
+ * This file is part of the ZCE Preparation Exam package.
+ *
+ * (c) Daniel Gomes <me@danielcsgomes.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace ZCE\PHPBasics;
 
 class PHPBasics
