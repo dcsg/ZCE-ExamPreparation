e2cb553828110bce22d9dcb6180a8beec58d44a7
Daniel Gomes <me@danielcsgomes.com>
2012-05-14 00:19:59 +0100

    will create some examples for the different types of variables

---
+++ src/ZCE/PHPBasics/Variables.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of the ZCE Preparation Exam package.
+ *
+ * (c) Daniel Gomes <me@danielcsgomes.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace ZCE\PHPBasics;
+
+/**
+ * Variables types and scope
+ *
+ * @author Daniel Gomes <me@danielcsgomes.com>
+ */
+class Variables
+{
+
+}
