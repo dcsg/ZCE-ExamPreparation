6b0805d74ab823b91ad1ca47520ceb698fdd7840
Daniel Gomes <me@danielcsgomes.com>
2012-05-14 22:38:09 +0100

    added new examples to Globals and Static variables

--- src/ZCE/PHPBasics/Variables.php
+++ src/ZCE/PHPBasics/Variables.php
@@ -15,20 +15,42 @@ namespace ZCE\PHPBasics;
  * @author Daniel Gomes <me@danielcsgomes.com>
  */
 
-class Variables
+/**
+ * Globals variables
+ * url: http://php.net/manual/en/language.variables.scope.php
+ *
+ */
+function globals()
 {
-    public function __construct()
-    {
-        //Globals
-        $GLOBALS["sum"] = $GLOBALS["x"]+$GLOBALS["y"];
-    }
+
+    // Using PHP-defined $Globals Array
+    $GLOBALS["sum"] = $GLOBALS["x"]+$GLOBALS["y"];
+    printf("The Sum of %d with %d is: %d", $GLOBALS["x"],$GLOBALS["y"],$GLOBALS["sum"]);
+
+    // Using global keyword
+    global $mul, $x, $y;
+    $mul = $x * $y;
+    printf("<br/>The Multiplication of %d with %d is: %d", $x,$y ,$mul);
 }
 
+/**
+ * Usage of static variables inside functions
+ * url: http://php.net/manual/en/language.variables.scope.php
+ *
+ */
+function statics()
+{
+    static $a = 0;
+    $b = 0;
+    printf("<br/>Value of a: %d and b: %d", $a, $b);
+    $a++; $b++;
+}
 //Declared the values to the global variables
-$x = 10;
-$y = 20;
+$x = 10; $y = 20;
 
-$variablesTest = new Variables();
+globals(); // outputs the sum and multiplication of x and y
 
-// print the result - output 30
-print($sum);
+statics(); // outputs a = b = 0
+statics(); // outputs a = 1 & b = 0
+statics(); // outputs a = 2 & b = 0
+
